a0f1bf3c3b4f34131e98a9c1c57e0d0b1aab34ef
Arend van Beelen jr. <arend@arendjr.nl>
2025-02-22 20:40:11 +0100

    Level 47

--- src/levels.rs
+++ src/levels.rs
@@ -58,6 +58,7 @@ pub const LEVELS: &[(u16, &str)] = &[
     (44, include_str!("../assets/levels/level044")),
     (45, include_str!("../assets/levels/level045")),
     (46, include_str!("../assets/levels/level046")),
+    (47, include_str!("../assets/levels/level047")),
     (56, include_str!("../assets/levels/level056")),
     (57, include_str!("../assets/levels/level057")),
     (66, include_str!("../assets/levels/level066")),
